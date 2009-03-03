49e17b8aaadc6fbcc1f0230e5904c64f985c0590
Alan LaMielle <lamielle@cs.colostate.edu>
2009-03-03 14:29:24 -0700

    InputProvider renaming
    Renamed the file _input_provider.py to InputProvider.py
    Updated the associated init and wscripts to work with this change

--- src/aether/core/__init__.py
+++ src/aether/core/__init__.py
@@ -4,7 +4,7 @@ __doc__ = """Some documentation, per se"""
 
 from _module import AetherModule
 from _param_module import AetherParamModule
-from _input_provider import InputProvider
 from _driver import AetherDriver
+from InputProvider import InputProvider
 from CameraInputProvider import CameraInputProvider
 from FaceInputProvider import FaceInputProvider
