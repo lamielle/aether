fcbf866565c7b867b84a999fa0eff9e221a86754
Alan LaMielle <lamielle@cs.colostate.edu>
2009-03-14 11:50:09 -0600

    Added first pass for the rewritten Settings class
    aether.core.Settings now uses YAML for the file format rather than the
    INI style format ConfigParser uses.  This was mostly for the feature of
    yaml to automatically determine config value types and create the
    appropriate python object.  ConfigParser simply creates strings by
    default.  If you want a specific type, you have to know what type to
    parse out of the config file.  YAML does this automatically.
    
    A check for the yaml python module was added to the root wscript.
    
    The start of the 'run only this' script was added as bin/aether. This
    script will eventually be the only thing we need to run to start aether.
    This script intalls to ${PREFIX}/aether/bin/aether.

--- src/aether/core/Settings.py
+++ src/aether/core/Settings.py
@@ -1,32 +1,85 @@
 from __future__ import with_statement
-from ConfigParser import ConfigParser
+import yaml
 
 class Settings(object):
-	__slots__=('threshold','transform','min_area')
+	#Dummy class for storing settings section values
+	class values(object): pass
 
-	#Settings section/value names
-	_settings_section='Input Settings'
-	_setting_threshold='threshold'
-	_setting_transform='transform'
-	_setting_min_area='min_area'
+	#Non-settings fields
+	_non_settings=('debug','loaded','saved')
 
-	def __init__(self,threshold=0,transform=None,min_area=0):
-		self.threshold=threshold
-		self.transform=[] if None is transform else transform
-		self.min_area = min_area
+	def __init__(self,file_name=None,debug=False):
+		self.debug=debug
+		self.loaded=False
+		self.saved=False
+
+		#Load the given file name if one was given
+		if None!=file_name: self.load(file_name)
+
+	def __str__(self):
+		return yaml.dump(self._get_settings_dict(),default_flow_style=False)
 
 	def save(self,file_name):
-		config=ConfigParser()
-		config.add_section(self._settings_section)
-		config.set(self._settings_section,self._setting_threshold,self.threshold)
-		config.set(self._settings_section,self._setting_transform,self.transform)
-		config.set(self._settings_section,self._setting_min_area,self.min_area)
-		with file(file_name,'w') as f:
-			config.write(f)
+		if self.debug: print "Writing settings to file '%s'...\n"%(file_name)
+
+		try:
+			#Get a dictionary containing the current settings
+			settings=self._get_settings_dict()
+
+			#Write the given dictionary to the given file in YAML format
+			with file(file_name,'w') as f:
+				yaml.dump(settings,f,default_flow_style=False)
+
+			if self.debug: print "Wrote settings to file '%s'..."%(file_name)
+			if self.debug: print yaml.dump(settings,default_flow_style=False)
+		except TypeError,e:
+			print "Error: Cannot save to Aether YAML settings file '%s' (Exception: %s)."%(file_name,e)
 
 	def load(self,file_name):
-		config=ConfigParser()
-		config.read(file_name)
-		self.threshold=int(config.get(self._settings_section,self._setting_threshold))
-		self.transform=eval(config.get(self._settings_section,self._setting_transform))
-		self.min_area=int(config.get(self._settings_section,self._setting_min_area))
+		#Assume success in loading from the start
+		self.loaded=True
+
+		if self.debug: print "Loading settings from file '%s'...\n"%(file_name)
+
+		#Attempt to load the given file
+		try:
+			with file(file_name,'r') as f:
+				settings=yaml.load(f)
+		except TypeError,e:
+			print "Error: Cannot load YAML file '%s', please make sure this is a valid Aether YAML settings file (Exception: %s)."%(file_name,e)
+			self.loaded=False
+		except yaml.scanner.ScannerError,e:
+			print "Error: Invalid YAML syntax in file '%s' (Exception: %s)."%(file_name,e)
+			self.loaded=False
+
+		#Only try to load the values from 'settings' if loading was successful
+		if self.loaded:
+			self._set_settings_from_dict(settings)
+
+	#Create a dictionary from the fields of 'self'
+	def _get_settings_dict(self):
+		settings={}
+		for section,values in self.__dict__.items():
+			#Fields named in self._non_settings should not be treated as settings values
+			if section not in self._non_settings:
+				settings[section]={}
+				for name,value in values.__dict__.items():
+					settings[section][name]=value
+		return settings
+
+	#Populate fields of 'self' using the entries in the given dictionary
+	def _set_settings_from_dict(self,settings_dict):
+		try:
+			for section,values in settings_dict.items():
+				if self.debug: print "Found Section '%s':"%(section)
+				setattr(self,section,Settings.values())
+				try:
+					for name,value in values.items():
+						setattr(getattr(self,section),name,value)
+						if self.debug: print "|-Found value: %s: %s (%s)"%(name,repr(value),type(value))
+				except AttributeError,e:
+					print "Error: Incorrect settings file format: All YAML name/value pairs must specify a Python dictionary (Exception: %s)."%(e)
+					self.loaded=False
+		except AttributeError,e:
+			print "Error: Incorrect settings file format: All YAML settings sections must specify a Python dictionary (Exception: %s)."%(e)
+			self.loaded=False
--- wscript
+++ wscript
@@ -8,13 +8,17 @@ def set_options(opt):
 	opt.tool_options('python')
 
 	opt.sub_options('src')
+	opt.sub_options('bin')
 
 def configure(conf):
 	#Configure python
 	conf.check_tool('python')
 	conf.check_python_version((2,4,2))
+	conf.check_python_module('yaml')
 
 	conf.sub_config('src')
+	conf.sub_config('bin')
 
 def build(bld):
 	bld.add_subdirs('src')
+	bld.add_subdirs('bin')
