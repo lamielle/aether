ac4fa2360d48198c8170822d929e12862d761ea7
Alan LaMielle <lamielle@cs.colostate.edu>
2009-04-02 10:32:40 -0600

    CVMatPyGameSurface cleanup

--- src/aether/transform/CVMatPyGameSurface.py
+++ src/aether/transform/CVMatPyGameSurface.py
@@ -20,22 +20,6 @@ class CVMatPyGameSurface(AetherTransform):
 		#Read the current frame from the flipped camera
 		frame=self.input.read()
 
-		#This is assuming an OpenCV camera backend
-		#In the future, it would be nice to be able to change out the camera backend
-
-		# scale the image to size of the window
-#		cvt_scale = cv.cvCreateImage(cv.cvSize(self.capture_dims[0],self.capture_dims[1]),frame.depth,frame.nChannels)
-#		cv.cvResize(frame,cvt_scale,cv.CV_INTER_LINEAR)
-
-		# need to convert the colorspace differently depending on where the image came from
-#		cvt_color = cv.cvCreateImage(cv.cvSize(cvt_scale.width,cvt_scale.height),cvt_scale.depth,3)
-#		if frame.nChannels == 3 : # image is BGR
-			# frame is in BGR format, convert it to RGB so the sky isn't orange
-#			cv.cvCvtColor(cvt_scale,cvt_color,cv.CV_BGR2RGB)
-#		elif frame.nChannels == 1 : # image is grayscale
-			# frame is grayscale
-#			cv.cvCvtColor(cvt_scale,cvt_color,cv.CV_GRAY2RGB)
-
 		# create a pygame surface
 		frame_surface=pygame.image.frombuffer(frame.imageData,(frame.width,frame.height),'RGB')
 
